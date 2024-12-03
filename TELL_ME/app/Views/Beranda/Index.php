18e0d6d53defcd19f39e66f5428c23f6d258e459
Banu Tasila <banufadel83@gmail.com>
2024-12-03 12:52:22 +0800

    wee

--- TELL_ME/app/Views/Beranda/Index.php
+++ TELL_ME/app/Views/Beranda/Index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Social Media UI</title>
-    <link rel="stylesheet" href="<?= base_url('bootstrap/css/stylelogin.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('bootstrap/css/styleBeranda.css') ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 <body>
